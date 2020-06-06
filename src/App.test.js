fdcf3f3d5f659ff9121269ada676dfcbf4ec4711
Bella Zhan <bellazhan2021@u.northwestern.edu>
2020-06-05 22:47:49 -0500

    APP TEST

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('TEST ONE', () => {
+	const test = 1;
+	expect(test).toEqual(1);
 });
